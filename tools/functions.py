eaba86926861ef6ff3890cad11f3abab13b557da
Andrea Mariani <ieo5776@ieo.it>
2024-09-16 17:27:28 +0200

    fixing indentation

--- tools/functions.py
+++ tools/functions.py
@@ -19,7 +19,7 @@ def mean_smoothing(k: int, time: int, signal: Iterable[float]) -> Iterable[int]:
 
       Returns:
         An Array 
-      """
+    """
 	# allocate filtSig
 	filtSig = np.zeros(time)
 	
@@ -32,58 +32,58 @@ def mean_smoothing(k: int, time: int, signal: Iterable[float]) -> Iterable[int]:
 def gaussian_smoothing(fwhm: int, signal: Iterable[float], time: int, 
 					   srate: int, k: int, check_kernel: bool = True
 					   ) -> Iterable[int]:
-	"""
-      Applies gaussian smoothing 
+    """
+    Applies gaussian smoothing 
 
-      Args:
-	  	fwhm: Full-width half-maximum value
-		signal: the raw signal to be smoothed
-		time: time domain of the time serie
-		srate: sampling rate
-	  	k: number of time points to consider before and after 
-		check_kernel: Boolen, if True check the fwhm is reasonable
+    Args:
+        fwhm: Full-width half-maximum value
+        signal: the raw signal to be smoothed
+        time: time domain of the time serie
+        srate: sampling rate
+        k: number of time points to consider before and after 
+        check_kernel: Boolen, if True check the fwhm is reasonable
 
-      Returns:
-          if check_kernel == False, returns the filtered signal, otherwise plots FWHM
-      """
+    Returns:
+	    if check_kernel == False, returns the filtered signal, otherwise plots FWHM
+    """
 	# prevents changing the original array
-	filtSignal = np.copy(signal)
+    filtSignal = np.copy(signal)
 	# convert k points to milliseconds, `t` in the gaussian formula
-	gtime = 1000*np.arange(-k, k+1)/srate
+    gtime = 1000*np.arange(-k, k+1)/srate
 	# create gaussian window
-	gauss_window = np.exp( -(4 * np.log(2) * gtime**2) / fwhm**2)
+    gauss_window = np.exp( -(4 * np.log(2) * gtime**2) / fwhm**2)
 	
 	# Check if fwhm makes sens
-	if check_kernel is True:
-		# Computre empirical fwhm
-		postFWHM = k + np.argmin( (gauss_window[k:] - 0.5)**2 )
-		preFWHM = np.argmin( (gauss_window - 0.5)**2 )
-		empFWHM = gtime[postFWHM] - gtime[preFWHM]
+    if check_kernel is True:
+        # Computre empirical fwhm
+        postFWHM = k + np.argmin( (gauss_window[k:] - 0.5)**2 )
+        preFWHM = np.argmin( (gauss_window - 0.5)**2 )
+        empFWHM = gtime[postFWHM] - gtime[preFWHM]
 
-		# Plot the FWHM
-		plt.plot(gtime, gauss_window, 'ko-')
-		plt.plot([gtime[preFWHM], gtime[postFWHM]], [gauss_window[preFWHM], gauss_window[postFWHM]], 'm')
-		plt.ylabel('Gain')
-		plt.title(f'Gaussian kernel with empirical FWHM: {empFWHM}')
-		plt.show()
-	else:
-		# normalize gaussian to unit energy
-		gauss_window = gauss_window / np.sum(gauss_window)
-		for i in range(k+1, time-k):
-			filtSignal[i] = np.sum(filtSignal[i-k:i+k+1] * gauss_window)
+        # Plot the FWHM
+        plt.plot(gtime, gauss_window, 'ko-')
+        plt.plot([gtime[preFWHM], gtime[postFWHM]], [gauss_window[preFWHM], gauss_window[postFWHM]], 'm')
+        plt.ylabel('Gain')
+        plt.title(f'Gaussian kernel with empirical FWHM: {empFWHM}')
+        plt.show()
+    else:
+        # normalize gaussian to unit energy
+        gauss_window = gauss_window / np.sum(gauss_window)
+        for i in range(k+1, time-k):
+            filtSignal[i] = np.sum(filtSignal[i-k:i+k+1] * gauss_window)
 
-	return filtSignal
+    return filtSignal
 
 def TKEO_denoising(signal: Iterable[float]) -> Iterable[float]:
 	"""
       Applies TKE operaton to a time serie
 
       Args:
-		signal: the raw signal to be smoothed
+        signal: the raw signal to be smoothed
 
       Returns:
-          denoised signal
-      """
+        denoised signal
+    """
 	filtSignal = np.copy(signal)
 	# TKEO operator
 	filtSignal[1:-1] = filtSignal[1:-1]**2 - filtSignal[0:-2]*filtSignal[2:]
@@ -96,13 +96,13 @@ def median_filter(threshold: int, signal: Iterable[float],
       Applies median filter to random spikes in the signal
 
       Args:
-	  	threshold: An int that specify the transitioning bound from signal to random spike
-		signal: the raw signal to be smoothed
-	  	k: number of time points to consider before and after
+        threshold: An int that specify the transitioning bound from signal to random spike
+        signal: the raw signal to be smoothed
+        k: number of time points to consider before and after
 
       Returns:
-	  	Smoothed signal with spikes removed
-      """
+        Smoothed signal with spikes removed
+    """
 	filtSignal = np.copy(signal)
 	thresh_over = np.where(filtSignal > threshold)[0]
 
@@ -123,14 +123,14 @@ def polynomial_fit(orders: Iterable[int], signal: Iterable[float],
 	  with Bayes information criterion
 
       Args:
-		orders: Range of Ints, corresponds to orders of polynomial to test
-		signal: the raw signal to be detrend.
-		n: number of data points in the signal
-		check_BIC: boolean, whether to plot of not the BIC plot for visual inspection
+        orders: Range of Ints, corresponds to orders of polynomial to test
+        signal: the raw signal to be detrend.
+        n: number of data points in the signal
+        check_BIC: boolean, whether to plot of not the BIC plot for visual inspection
 
       Returns:
-	  	Smoothed signal with spikes removed
-      """
+        Smoothed signal with spikes removed
+    """
 	# crete MSE object
 	epsilon = np.zeros(len(orders))
 	
@@ -172,13 +172,13 @@ def time_synch_averaging(signal: Iterable[float], n_events: int, k: int,
       Averages multiple epochs of some signal to increase S/N ratio
 
       Args:
-		signal: The raw signal to be averaged
-		n_events: Number of repetitive events in the singal to be averaged
-		k: Fundamental param, duration of the event in time (in data indx, not ms)
-		onset_time: Idx of singal at which the event occurs
+        signal: The raw signal to be averaged
+        n_events: Number of repetitive events in the singal to be averaged
+        k: Fundamental param, duration of the event in time (in data indx, not ms)
+        onset_time: Idx of singal at which the event occurs
 
       Returns:
-	  Signal with events averaged
+        Signal with events averaged
     """
 	mat = np.zeros((n_events, k))
 
@@ -195,11 +195,11 @@ def least_square_template_matching(signal: Iterable[float], artifact: Iterable[f
       Removes artifacts from a signal via least-square template matching over an artifact signal
 
       Args:
-		signal: The raw data channel/signal of interest
-		artifact: The raw artifcal channel/signal that captures the artifact leaked into the data signal
+        signal: The raw data channel/signal of interest
+        artifact: The raw artifcal channel/signal that captures the artifact leaked into the data signal
 
       Returns:
-	  Residuals of signal channel
+        Residuals of signal channel
     """
 	# Set matrix size
 	DM = np.shape(signal)
